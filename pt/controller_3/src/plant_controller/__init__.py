03c670b3b8a81d774b73bc9675329496ee6a91ef
DisasterlyDisco <aleksander.r.m@protonmail.com>
2026-04-18 19:56:49 +0200

    Cleaned up main, added greenhouse module

--- pt/controller_3/src/plant_controller/__init__.py
+++ pt/controller_3/src/plant_controller/__init__.py
@@ -2,40 +2,12 @@ import sys, random
 from . import datapoint, database, web_api
 
 def main():
-    #conf = datapoint.Confidence(2.2, 0.92)
-    #print(conf.str_representation())
-    #k = datapoint.Datapoint("Moisture", 55.02, conf, "%")
-    #print(f'{k.parameter} reading: {k.value}±{k.confidence.interval}{k.units} with {k.confidence.level}% confidence')
-    #print("Aaaaaand we are OFF!! :D")
-    #print(sys.argv[0])
-    #print()
-    #print()
-
-    #unit = "THEMOON"
 
     print("Setting up DB")
     db = database.Database(
         token=sys.argv[1]
     )
     print("DB Done!")
-
-    #print("Spawning client")
-    #client = db.spawn_client()
-    #print("client Done!")
-
-    #for x in range (2):
-    #    print("----------------------------------------------")
-    #    print(f'Measurement {x}')
-    #    value = random.random() * 100
-    #    k = datapoint.Datapoint("Moisture", value, conf, "%")
-    #    print("writing measurement to DB")
-    #    client.write_measurement(unit, k)
-    #    print("writing done!")
-    #    print()
-    #    print("Reading all from db")
-    #    df = client.read_all(unit, "Moisture")
-
-    #    print(df)
     
     print("Spinning up web API")
     api = web_api.WebAPI(
