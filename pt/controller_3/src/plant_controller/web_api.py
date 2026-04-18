b5b609bfe136f58081dd87154f13c8b4b66b5c26
DisasterlyDisco <aleksander.r.m@protonmail.com>
2026-04-18 17:50:00 +0200

    Removed timestamp check from web api read temporarily

--- pt/controller_3/src/plant_controller/web_api.py
+++ pt/controller_3/src/plant_controller/web_api.py
@@ -1,11 +1,11 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Query
 from fastapi.responses import JSONResponse
 from pandas import DataFrame
 
 from .database import Database
 
-from typing import Dict, Any
+from typing import Dict, Any, Annotated
 
 class WebAPI:
     def __init__(self, host: str, port: int, database: Database):
@@ -30,11 +30,27 @@ class WebAPI:
         async def fetch(
             unit: str,
             parameter: str,
-            limit: int | None = None,
-            since_timestamp: str | None = None
+            limit: Annotated[
+                int | None,
+                Query(
+                    description="The maximum number of measurements to return. If not provided, all measurements will be returned."
+                )
+            ] = None,
+            since_timestamp: Annotated[
+                str | None,
+                Query(
+                    description="Only return measurements taken after this timestamp. If not provided, all measurements will be returned regardless of timestamp."
+                )
+            ] = None
         ) -> JSONResponse:
-            if since_timestamp != None:
-
+            #if since_timestamp != None:
+            #    try:
+            #        DataFrame([since_timestamp], columns=["timestamp"], dtype="datetime64[ns]")
+            #    except Exception as e:
+            #        return JSONResponse(
+            #            status_code=400,
+            #            content={"error": f"Invalid timestamp format: {e}"}
+            #        )
             client = self.database.spawn_client()
             dataframe = client.read_measurements(unit, parameter, limit, since_timestamp)
             return dataframe.to_dict(orient="records")
